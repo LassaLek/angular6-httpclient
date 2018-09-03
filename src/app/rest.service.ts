d4fba101011f2c1152f7044439ad0b3ab05c745a
Petr Hoza <hozapetr@email.cz>
2018-09-03 21:38:49 +0100

    finish service

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -26,22 +26,58 @@ export class RestService {
 
   public getProduct(id: Number): Observable<any> {
     return this.http
-      .get(`${endpoint}product/${id}`)
+      .get(`${endpoint}products/${id}`)
       .pipe(map(this.extractData));
   }
 
-  public addProduct(product): Observable<any> {
+  public addProduct(product: ProductModel): Observable<any> {
     return this.http
       .post<any>(endpoint + 'products', JSON.stringify(product), httpOptions)
       .pipe(
         map(
           tap(p => console.log(`added product with id ${p.id}`)),
-          catchError()
+          catchError(this.handleError<any>('addProduct'))
         )
       );
+  }
+
+  public updateProduct(id: Number, product: ProductModel): Observable<any> {
+    return this.http
+      .put(`${endpoint}product/${id}`, JSON.stringify(product), httpOptions)
+      .pipe(
+        map(
+          tap(_ => console.log(`updated product with id ${id}`)),
+          catchError(this.handleError<any>('updateProduct'))
+        )
+      );
+  }
+
+  public deleteProduct(id: Number): Observable<any> {
+    return this.http
+      .delete<any>(`${endpoint}product/${id}`, httpOptions)
+      .pipe(        map(
+        tap(_ => console.log(`deleted product with id ${id}`)),
+        catchError(this.handleError<any>('deleteProduct'))
+      ));
   }
 
   private extractData(res: Response) {
     return res || {};
   }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<any> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
+  }
+}
+
+interface ProductModel {
+  id: Number;
 }
